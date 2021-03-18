081de8a77608f29f1e41e68acc854e80c474dac2
j-asif <jalal.asif97@gmail.com>
2021-03-18 13:26:26 -0300

    modified urls

--- djangohelloworld/djangohelloworld/urls.py
+++ djangohelloworld/djangohelloworld/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from hello.views import myView
+from hello.views import myView 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
